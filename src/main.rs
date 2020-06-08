44a0ad50354c5ccaa32ed0a10a199dc62d2a309b
qiaojinxia <1158829384@qq.com>
2020-06-08 23:55:41 +0800

    update cmd parse

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,41 @@ use crate::classpath::cp::ClassPath;
 
 fn main() {
 
-    //获取系统参数
-    let args: Vec<String> = env::args().collect();
-    let mut classpath: &str = "./*";
-    let mut xjre: &str = "";
-    let mut classname = args[1].as_str();
+    let lenlast = env::args().len()-1;
+    let classname ;
+    let mut args: Vec<String>;
+    if  lenlast  > 0 {
+
+        //过滤掉最后一个参数
+        args = env::args(). enumerate()
+            . filter_map(|(i, e)| if i!= lenlast { Some(e) } else { None })
+            . collect();
+        args.push("-h".parse().unwrap());
+        args.push("".parse().unwrap());
+
+        classname = env::args().last().unwrap();
+
+    }else{
+
+        args = env::args().collect();
+        args.push("--help".parse().unwrap());
+        println!("please type classname!");
+        classname = "".parse().unwrap();
+    }
+
+    println!("{:?}",args);
     let matches ;
-    if args.len() >  2  as usize {
         //创建一个 命令行解析器
          matches = App::new("gravitational waves	virtual machine")
             .version("0.0.0.1")
             .author("Caomao.Boy <1158829384@qq.com>")
             .about("A JVM Implement By Rust")
             .arg(Arg::with_name("classpath")
-                .short("c")
+                .short("p")
                 .long("classpath")
                 .value_name("UserClassPath")
                 .help("Set a classpath to java env ")
-                .multiple(true)
+                .multiple(false)
                 .takes_value(true))
             .arg(Arg::with_name("Xjre")
                 .long("Xjre")
@@ -38,22 +55,26 @@ fn main() {
                 .multiple(true)
                 .takes_value(true)
                 .help("Set java Runtime env"))
-            .get_matches();
+             .arg(Arg::with_name("placeholder")
+                 .long("placeholder")
+                 .short("h")
+                 .hidden(true)
+                 .empty_values(true)
+                 .value_name("placeholder")
+                 .help("placeholder"))
+            .get_matches_from(args);
 
         // Gets a value for config if supplied by user, or defaults to "default.conf"
-        classpath = matches.value_of("classpath").unwrap_or("./*");
-
+        let classpath = matches.value_of("classpath").unwrap_or("./*");
         // Gets a value for config if supplied by user, or defaults to "default.conf"
-        xjre = matches.value_of("Xjre").unwrap_or("");
-        classname =args[3].as_str().trim();
+        let xjre = matches.value_of("Xjre").unwrap_or("");
 
-    }
-
+    // let classname =args[args.len()-1].as_str().trim();
     println!("Input ClassName = {}",classname);
     //"java.lang.Object"
     //替换通配符 ./为当前路径
     let rf = ClassPath::new_class_path(xjre, classpath);
-    let bytecode = rf.read_class(classname);
+    let bytecode = rf.read_class(classname.as_ref());
     println!("{:?}",bytecode);
     // let rf = classdir::newClassDir(classpath);
     // //如果是 . 就获取当前目录
